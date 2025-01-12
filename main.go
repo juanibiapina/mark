0e887ac203229e8587ce4e506d70cd29badff2ae
Juan Ibiapina <juanibiapina@gmail.com>
2025-01-12 22:19:58 +0100

    feat: Initial flow for talking to openAI API

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"os"
@@ -10,6 +11,7 @@ import (
 	"github.com/charmbracelet/bubbles/textarea"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/openai/openai-go"
 )
 
 func main() {
@@ -19,6 +21,29 @@ func main() {
 	}
 }
 
+func userMessage(text string) tea.Cmd {
+	return func() tea.Msg {
+		client := openai.NewClient()
+
+		ctx := context.Background()
+
+		completion, err := client.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
+			Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
+				openai.UserMessage(text),
+			}),
+			Seed:  openai.Int(1),
+			Model: openai.F(openai.ChatModelGPT4o),
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		return replyMessage(completion.Choices[0].Message.Content)
+	}
+}
+
+type replyMessage string
+
 type ChatMessage struct {
 	title, desc string
 }
@@ -27,8 +52,8 @@ func (i ChatMessage) FilterValue() string { return "" }
 
 type itemDelegate struct{}
 
-func (d itemDelegate) Height() int { return 1 }
-func (d itemDelegate) Spacing() int { return 0 }
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
 func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
 func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
 	i, ok := listItem.(ChatMessage)
@@ -84,6 +109,12 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+
+	case replyMessage:
+		m.list.InsertItem(len(m.list.Items()), ChatMessage{title: "AI", desc: string(msg)})
+
+		return m, nil
+
 	case tea.WindowSizeMsg:
 		textAreaHeight := m.textarea.Height()
 
@@ -92,6 +123,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.textarea.SetWidth(msg.Width)
 
 		return m, nil
+
 	case tea.KeyMsg:
 		switch msg.String() {
 
@@ -108,7 +140,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 			m.textarea.Reset()
 
-			return m, m.list.InsertItem(len(m.list.Items()), ChatMessage{title: "You", desc: v})
+			return m, tea.Batch(m.list.InsertItem(len(m.list.Items()), ChatMessage{title: "You", desc: v}), userMessage(v))
 
 		default:
 			// Send all other keypresses to the textarea.
