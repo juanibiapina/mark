49dce9752779f484b3a9a666331a038809860e2b
Juan Ibiapina <juanibiapina@gmail.com>
2025-01-19 13:13:30 +0100

    dev: Enable debug logging to a file from tea

--- main.go
+++ main.go
@@ -10,6 +10,15 @@ import (
 )
 
 func main() {
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
 	p := tea.NewProgram(app.MakeApp(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Oof: %v\n", err)
