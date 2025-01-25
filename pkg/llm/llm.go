7be41ac274bcd02dea12e32d8544c0f6c60c780c
Juan Ibiapina <juanibiapina@gmail.com>
2025-01-25 09:19:50 +0100

    refactor: Move Conversation to a new file

--- pkg/llm/llm.go
+++ pkg/llm/llm.go
@@ -11,36 +11,6 @@ const (
 	RoleAssistant
 )
 
-type Conversation struct {
-	messages         []Message
-	StreamingMessage *StreamingMessage
-}
-
-func (c *Conversation) CancelStreaming() {
-	if c.StreamingMessage == nil {
-		return
-	}
-
-	c.StreamingMessage.Cancel()
-
-	// Add the partial message to the chat history
-	c.AddMessage(Message{Role: RoleAssistant, Content: c.StreamingMessage.Content})
-
-	c.StreamingMessage = nil
-}
-
-func (c *Conversation) AddMessage(m Message) {
-	c.messages = append(c.messages, m)
-}
-
-func (c *Conversation) Messages() []Message {
-	return c.messages
-}
-
-func (c *Conversation) Reset() {
-	c.messages = []Message{}
-}
-
 type Message struct {
 	Role    Role
 	Content string
