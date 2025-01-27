472f323e55a3ff7f25dcf8ad624587ec87e888d8
Juan Ibiapina <juanibiapina@gmail.com>
2025-01-27 22:40:59 +0100

    chore: Integrate cobra for parsing CLI arguments

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/dlclark/regexp2 v1.11.0 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/gorilla/css v1.0.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
@@ -31,6 +32,8 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.3-0.20240618155329-98d742f6907a // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
--- main.go
+++ main.go
@@ -7,20 +7,32 @@ import (
 	"ant/pkg/app"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	if len(os.Getenv("DEBUG")) > 0 {
-		f, err := tea.LogToFile("debug.log", "debug")
-		if err != nil {
-			fmt.Println("fatal:", err)
-			os.Exit(1)
-		}
-		defer f.Close()
+	rootCmd := &cobra.Command{
+		Use:   "mark",
+		Short: "Mark TUI Assistant",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(os.Getenv("DEBUG")) > 0 {
+				f, err := tea.LogToFile("debug.log", "debug")
+				if err != nil {
+					fmt.Println("fatal:", err)
+					os.Exit(1)
+				}
+				defer f.Close()
+			}
+
+			p := tea.NewProgram(app.MakeApp(), tea.WithAltScreen())
+			if _, err := p.Run(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			}
+		},
 	}
 
-	p := tea.NewProgram(app.MakeApp(), tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
 	}
 }
