e6712414f9ac81250d3942dccd5bfbc9a1bcfaed
Juan Ibiapina <juanibiapina@gmail.com>
2025-02-07 02:02:07 +0100

    refactor: Remove dead code

--- pkg/app/messages.go
+++ pkg/app/messages.go
@@ -1,7 +1,5 @@
 package app
 
-import tea "github.com/charmbracelet/bubbletea/v2"
-
 type (
 	replyMessage   string
 	partialMessage string
@@ -10,9 +8,3 @@ type (
 type errMsg struct{ err error }
 
 func (e errMsg) Error() string { return e.err.Error() }
-
-func message(msg tea.Msg) tea.Cmd {
-	return func() tea.Msg {
-		return msg
-	}
-}
