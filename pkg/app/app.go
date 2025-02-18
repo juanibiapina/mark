c0a9b42387c10061cad656bb54f06cea2d021d32
Juan Ibiapina <juanibiapina@gmail.com>
2025-02-18 07:32:40 +0100

    refactor: Move commands to another file

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -296,59 +296,6 @@ func (m *App) newConversation() {
 	m.focused = FocusedInput
 }
 
-func (m *App) submitMessage() tea.Cmd {
-	m.cancelStreaming()
-
-	v := m.input.Value()
-	if v != "" {
-		m.conversation.AddMessage(model.Message{Role: model.RoleUser, Content: v})
-		m.input.Reset()
-	}
-
-	// maybe update the prompt here
-
-	m.startStreaming()
-
-	cmds := []tea.Cmd{
-		complete(m),      // call completions API
-		processStream(m), // start receiving partial message
-	}
-	return tea.Batch(cmds...)
-}
-
-func complete(m *App) tea.Cmd {
-	return func() tea.Msg {
-		err := m.ai.CompleteStreaming(&m.conversation, m.stream)
-		if err != nil {
-			return errMsg{err}
-		}
-
-		return nil
-	}
-}
-
-func processStream(m *App) tea.Cmd {
-	return func() tea.Msg {
-		select {
-		case v := <-m.stream.Reply:
-			return replyMessage(v)
-		case v := <-m.stream.Chunks:
-			return partialMessage(v)
-		}
-	}
-}
-
-func (m *App) saveConversation() tea.Cmd {
-	return func() tea.Msg {
-		err := m.db.SaveConversation(m.conversation)
-		if err != nil {
-			return errMsg{err}
-		}
-
-		return nil
-	}
-}
-
 func (m *App) renderConversation() {
 	messages := m.conversation.Messages
 
--- pkg/app/commands.go
+++ pkg/app/commands.go
@@ -1,8 +1,21 @@
 package app
 
 import (
+	"mark/pkg/model"
+
 	tea "github.com/charmbracelet/bubbletea/v2"
 )
+
+func (m *App) saveConversation() tea.Cmd {
+	return func() tea.Msg {
+		err := m.db.SaveConversation(m.conversation)
+		if err != nil {
+			return errMsg{err}
+		}
+
+		return nil
+	}
+}
 
 func (m *App) loadConversations() tea.Cmd {
 	return func() tea.Msg {
@@ -12,5 +25,47 @@ func (m *App) loadConversations() tea.Cmd {
 		}
 
 		return conversationEntriesMsg(conversations)
+	}
+}
+
+func (m *App) submitMessage() tea.Cmd {
+	m.cancelStreaming()
+
+	v := m.input.Value()
+	if v != "" {
+		m.conversation.AddMessage(model.Message{Role: model.RoleUser, Content: v})
+		m.input.Reset()
+	}
+
+	// maybe update the prompt here
+
+	m.startStreaming()
+
+	cmds := []tea.Cmd{
+		complete(m),      // call completions API
+		processStream(m), // start receiving partial message
+	}
+	return tea.Batch(cmds...)
+}
+
+func complete(m *App) tea.Cmd {
+	return func() tea.Msg {
+		err := m.ai.CompleteStreaming(&m.conversation, m.stream)
+		if err != nil {
+			return errMsg{err}
+		}
+
+		return nil
+	}
+}
+
+func processStream(m *App) tea.Cmd {
+	return func() tea.Msg {
+		select {
+		case v := <-m.stream.Reply:
+			return replyMessage(v)
+		case v := <-m.stream.Chunks:
+			return partialMessage(v)
+		}
 	}
 }
